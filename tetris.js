72b98a35d402bbd992765576f98c51db81e9a216
zergunchic <32296377+zergunchic@users.noreply.github.com>
2019-11-19 20:32:07 +0500

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -6,36 +6,44 @@ var orangeTile = new Image();
 var greenTile = new Image();
 var tilesArray = [blueTile,redTile,orangeTile,greenTile];
 var currentObjectsToDraw = [];
+
+//Default tetris fild size - 20 x 10 (22 x 10)
 var tetrisMap = Array(30);
+for (var i = 0; i < tetrisMap.length; i++) { 
+    tetrisMap[i] = Array(10); 
+} 
 // INITIAL BLOCK
 canvas.width  = 320;
 canvas.height = 700;
 loadResources();
+
+
 // CLASS BLOCK
 class Cube{
 	constructor(){
-		this.tileColor = tilesArray[0];
-		this.posX = 0;
-		this.posY = 0;
+		let randomNumberForColorChoose = Math.floor(Math.random()*4);
+		this.tileColor = tilesArray[randomNumberForColorChoose];
 		this.elements=[];
+		this.elements.push(new Element(5,0,this.tileColor));
+		this.elements.push(new Element(6,0,this.tileColor));
+		this.elements.push(new Element(5,1,this.tileColor));
+		this.elements.push(new Element(6,1,this.tileColor));
 	}
 	paint(){
-		for (element of this.elements){
-			ctx.drawImage()
+		for (let element of this.elements){
+			ctx.drawImage(element.tileColor, element.posX, element.posY);
 		}
-		ctx.drawImage(this.tileColor, this.posX, this.posY);
-		ctx.drawImage(this.tileColor, this.posX+16, this.posY+16);
-		ctx.drawImage(this.tileColor, this.posX+16, this.posY);
-		ctx.drawImage(this.tileColor, this.posX, this.posY+16);
-		this.gravity();
-	}
-	gravity(){
-		this.posY+=16;
 	}
 }
 class Element{
-	this.posX = 0;
-	this.posY = 0;
+	constructor(tileX,tileY, tileColor){
+	this.tileX = tileX;
+	this.tileY = tileY;
+	this.posX = tileX*16;
+	this.posY = tileY*16;
+	this.tileColor = tileColor;
+	}
+
 }
 
 //MAIN LOOP BISENESS LOGIC
@@ -55,6 +63,8 @@ function render(){
 	setInterval(function(){drawObjects()}, 500);
 }
 
+
+// LOADING IMAGES
 function loadResources(){
 	blueTile.src = 'resources/blue_tile16.png';
 	redTile.src = 'resources/red_tile16.png';
